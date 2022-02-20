49bc529779a15fefa537d2e0be08f2df05602f17
nikolav <admin@nikolav.rs>
2022-02-20 20:29:19 +0000

    #5

--- app.js
+++ app.js
@@ -2,24 +2,19 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const cors = require("cors");
+// const cors = require("cors");
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use((req, res, next) => {
-  return res.send("howdy");
-});
-
-// app.use(logger('dev'));
-// app.use(logger("combined"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.resolve(__dirname, "public")));
+app.use(logger("combined"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 // app.use(cors());
+app.use(express.static(path.resolve(__dirname, "public")));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
